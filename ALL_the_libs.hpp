b3208b3dc4e06dcb6fdce994b02f29a54200dca3
Wojciech Siewierski <darkvifon@gmail.com>
2012-03-12 16:25:12 +0100

    a seperate header

---
+++ ALL_the_libs.hpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <deque>
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <set>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <cassert>
+#include <cstdlib>
+#include <cstring>
+#include <ncurses.h>
+#include <unistd.h>
+
+#ifdef __GXX_EXPERIMENTAL_CXX0X__
+#include <array>
+#include <functional>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#endif
--- test.cpp
+++ test.cpp
@@ -1,26 +1,7 @@
-#include <algorithm>
-#include <cstdlib>
-#include <deque>
-#include <fstream>
-#include <iostream>
-#include <list>
-#include <map>
-#include <memory>
-#include <set>
-#include <sstream>
-#include <string>
-#include <unistd.h>
-#include <vector>
+#include <ALL_the_libs>
 
-#ifdef __GXX_EXPERIMENTAL_CXX0X__
-#include <array>
-#include <functional>
-#include <tuple>
-#include <unordered_map>
-#include <unordered_set>
-#endif
-
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     
     return 0;
 }
